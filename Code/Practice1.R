1fcd950a5cec61966a2d2fd72ba70963c9dbd63b
Smolyn <DS93656@AD1.PROD>
2019-10-29 11:13:54 -0400

    First round of updates

---
+++ Code/Practice1.R
@@ -0,0 +1,9 @@
+#This is me testing making sure I still know how to write code and push it to GitHub
+#Start by initializing some tables and variables
+
+user <- "Danielle"
+user2 <- "Trevor"
+
+x <- 5
+y <- 5.4
+z <- x + y
